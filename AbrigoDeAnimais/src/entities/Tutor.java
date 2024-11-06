5b41b523f6d4db36cd81563b0a90c61274b677f1
WesleySales <126846974+WesleySales@users.noreply.github.com>
2024-11-05 21:04:56 -0300

    Add files via upload

---
+++ AbrigoDeAnimais/src/entities/Tutor.java
@@ -0,0 +1,107 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tutor {
+   private static int geradorID=1;
+   private int id; 
+   private String nome;
+   private String sobrenome;
+   private String telefone;
+   private int idade;
+   private static List<Tutor> listaDeTutores = new ArrayList<>();
+
+    public Tutor( String nome, String sobrenome, String telefone, int idade) {
+        this.id = geradorID++;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+        this.idade = idade;
+    }
+   
+    public static void cadastrarTutor( String nome, String sobrenome, String telefone, int idade){
+        Tutor tutor = new Tutor(nome, sobrenome, telefone, idade);
+        listaDeTutores.add(tutor);
+    }
+    
+    public void deletarTutor(int id){
+        Tutor t = buscarTutorPorId(id);
+        listaDeTutores.remove(t);
+    }
+    
+    public Tutor buscarTutorPorId(int id){
+        if(!listaDeTutores.isEmpty()){
+            for(Tutor t: listaDeTutores){
+                if(t.getId()==id){
+                    return t;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static String exibirTutores(){
+        StringBuilder sb = new StringBuilder();
+        if(!listaDeTutores.isEmpty()){
+           for(Tutor t: listaDeTutores){
+               sb.append(t).append("\n");
+           }
+        }
+        return sb.toString();
+    }
+    
+    public static int getGeradorID() {
+        return geradorID;
+    }
+
+    public static void setGeradorID(int geradorID) {
+        Tutor.geradorID = geradorID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "id:" + id + ", Tutor: " + nome + " " + sobrenome + ", Telefone=" + telefone + ", Idade=" + idade;
+    }
+    
+}
